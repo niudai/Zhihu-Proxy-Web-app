a6b883d2bddfd8e06dd9bcb6a3d5e65da58a8eab
niudai <1244453736@qq.com>
2019-09-17 15:11:38 +0800

    added html-parser dependency

--- routes/answser.js
+++ routes/answser.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var httpClient = require('request');
-
+var HtmlParser = require('node-html-parser');
 let questionId = '345195246';
 
 let answerId = '819299062';
@@ -11,11 +11,14 @@ const answerAPI = `https://www.zhihu.com/question/${questionId}/answer/${answerI
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  httpClient(answerAPI, { json: true }, (err, _res, body) => {
-    console.log(body);
-    res.render('answer', { title: body.subject,  answer: body.description_html})
-    console.log(body.subject)
-  })
+  httpClient(answerAPI, { }, (err, _res, body) => {
+    
+    // console.log(body);
+    const root = HtmlParser.parse(body);
+    console.log(root.firstChild.structure);
+    console.log(root.querySelector('#js-initialData').);
+    res.render('answer', { title: '答案',  answer: body})
+  });
 });
 
 
