bb717fa0fa53706e87504a120713506bc3278f52
niudai <1244453736@qq.com>
2019-09-20 22:12:58 +0800

    released hard-coded param in question api

--- routes/answser.js
+++ routes/answser.js
@@ -2,24 +2,22 @@ var express = require('express');
 var router = express.Router();
 var httpClient = require('request');
 var HtmlParser = require('node-html-parser');
-let questionId = '327155365';
 
 let answerId = '819299062';
 let offset = 0;
 let includeContent = 'data[*].is_normal,content;';
 
-const answerAPI = `https://www.zhihu.com/api/v4/questions/${questionId}/answers?include=${includeContent}?offset=${offset}`
-
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:questionId', function(req, res, next) {
+  let questionId = req.params.questionId;
+  let answerAPI = `https://www.zhihu.com/api/v4/questions/${questionId}/answers?include=${includeContent}?offset=${offset}`
   httpClient(answerAPI, {json: true }, (err, _res, body) => {
     console.log(answerAPI);
     console.log(body);
-    let answers = body.data
-    let title = body.data[0].question.title
-    console.log(answers);
-    res.render('answer', { title,  answers: answers})
+    res.send(body.data);
+    // let title = body.data[0].question.title
+    // console.log(answers);
+    // res.render('answer', { title,  answers: answers})
   });
 });
 
