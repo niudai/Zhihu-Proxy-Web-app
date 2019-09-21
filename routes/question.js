ebd274bbd2457a1337ff06692b51b6c79e72e37e
niudai <1244453736@qq.com>
2019-09-21 21:45:39 +0800

    changed img src attribute to native

--- routes/question.js
+++ routes/question.js
@@ -2,28 +2,46 @@ var express = require('express');
 var router = express.Router();
 var httpClient = require('request');
 var HtmlParser = require('node-html-parser');
+var KeyValueParser = require('../util/keyValueParser');
 
 let answerId = '819299062';
 let offset = 0;
 let includeContent = 'data[*].is_normal,content;';
 
 /* GET home page. */
-router.get('/:questionId', function(req, res, next) {
+router.get('/:questionId', function (req, res, next) {
   let questionId = req.params.questionId;
   let answerAPI = `https://www.zhihu.com/api/v4/questions/${questionId}/answers?include=${includeContent}?offset=${offset}`
-  httpClient(answerAPI, {json: true }, (err, _res, body) => {
+  httpClient(answerAPI, { json: true }, (err, _res, body) => {
     console.log(answerAPI);
     // console.log(body);
     let answerHtmlRoots = body.data.map(answer => HtmlParser.parse(answer.content));
     // answerHtmlRoots.forEach(root => console.log(root.toString()));
-    let imgAttrs = answerHtmlRoots.filter(root => root.querySelector('img')).map(root => root.querySelector('img').rawAttributes);
-    imgAttrs.forEach(attr => console.log(attr['data-original']));
-    res.send(body.data);
-    // let title = body.data[0].question.title
-    // console.log(answers);
-    // res.render('answer', { title,  answers: answers})
+    let imgAttrs = answerHtmlRoots.filter(root => root.querySelector('img')).map(root => root.querySelectorAll('img'));
+    // console.log(imgAttrs);
+    imgAttrs.forEach(
+      attr => {
+        attr.forEach(img => {
+          // let parser = new KeyValueParser(img.rawAttrs, { quoted: '\"',  aync: false});
+          // let items = parser.run();
+          img.rawAttributes.src = img.rawAttributes['data-actualsrc'];
+          img.rawAttrs = `src = \" ${img.rawAttributes['data-actualsrc']} \"`;
+          // img.rawAttrs = `src = \"${items['data-original']}}\"`;
+          console.log(img.rawAttributes);
+          console.log(img.rawAttr);
+        }
+        );
+    });
+    // body.data.forEach((answer, index) => {
+    //   // answer.content = answerHtmlRoots[index].toString();
+    //   // console.log(answer.content);
+    //   console.log(answerHtmlRoots[index].toString());
+    // })
+    // console.log(imgAttrs);
+    // res.send(body.data);
+    res.send(answerHtmlRoots.toString());
   });
-});
+})
 
 
 
