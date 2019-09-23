3f8cfd0687d106d383ffec6ee97b5fd9250cf64b
niudai <1244453736@qq.com>
2019-09-23 23:12:16 +0800

    download picture succesfully

--- routes/picture.js
+++ routes/picture.js
@@ -3,20 +3,17 @@ var router = express.Router();
 var httpClient = require('request');
 var fs = require('fs');
 
-let offset = 0;
-let includeContent = 'data[*].is_normal,content;';
+const IMAGE_DIRCTORY = 'public/images'
+
 
 /* GET home page. */
-router.get('', function (req, res, next) {
+router.get(':imageName', function (req, res, next) {
+  let imageName = req.params['imageName'];
   let answerAPI = `https://pic3.zhimg.com/80/v2-d42f78b022ce1264cb45f3ce66835f74_hd.jpg`
   httpClient(answerAPI, { json: false }, (err, _res, body) => {
-    res.on
     res.set('Content-Type', 'image/jpeg');
-    var stream = fs.createWriteStream("test.jpg", { flags: 'a' });
-    stream.write(body);
-    res.status(200);
-    res.send(body);
-  });
+
+  }).pipe(fs.createWriteStream("test.jpg", { flags: 'a' });
 })
 
 
