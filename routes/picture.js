42d2d6c79cc937e52db072865c72381e3e68d080
niudai <1244453736@qq.com>
2019-09-24 20:32:00 +0800

    made picture loading work normally

--- routes/picture.js
+++ routes/picture.js
@@ -2,20 +2,24 @@ var express = require('express');
 var router = express.Router();
 var httpClient = require('request');
 var fs = require('fs');
+const path = require('path');
 
-const IMAGE_DIRCTORY = 'public/images'
+const IMAGE_DIRCTORY = '/images'
 
 /* GET home page. */
 router.get('/:imageName', function (req, res, next) {
   let imageName = req.params.imageName;
-  let answerAPI = `https://pic3.zhimg.com/80/${imageName}.jpg`
+  let answerAPI = `https://pic3.zhimg.com/80/${imageName}`
+  // httpClient(answerAPI, { json: false }, (err, _res, body) => {
+  // }).pipe(fs.createWriteStream(`${IMAGE_DIRCTORY}/${imageName}.jpg`, { flags: 'a' }))
   httpClient(answerAPI, { json: false }, (err, _res, body) => {
-    // res.set('Content-Type', 'image/jpeg');
-
-  }).pipe(fs.createWriteStream(`${imageName}.jpg`, { flags: 'a' }))
-  res.send('image stored');
+  }).pipe(res);
+  // res.sendFile(`${IMAGE_DIRCTORY}/${imageName}.jpg`);
+  // res.sendFile(path.resolve(`public/images/${imageName}.jpg`));
 })
 
-
+let callback = (dir, res) => {
+  
+}
 
 module.exports = router;
